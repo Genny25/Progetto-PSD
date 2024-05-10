3b733e6f606ccf52762f22d99bcd346ab1a4c1c0
Lorenzo046 <147921555+Lorenzo046@users.noreply.github.com>
2024-05-10 12:31:12 +0200

    Update data.c

--- c_file/data.c
+++ c_file/data.c
@@ -3,9 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include "utile.h"
-#include "data.h"
+#include "data.h"  // Include dichiarazioni per questo modulo
 
-struct DateTime
+// Definizione della struttura DateTime per rappresentare date e orari
+struct DateTime 
 {
     int year;
     int month;
@@ -14,20 +15,25 @@ struct DateTime
     int minutes;
 };
 
+// Funzione per creare una nuova data
 Date nuova_data(int year, int month, int day, int hour, int minutes)
 {
+    // Alloca memoria per la struttura DateTime
     Date date = malloc(sizeof(struct DateTime));
+    // Controlla se l'allocazione ha avuto successo
     if (date == NULL)
     {
         perror("Allocazione oggetto 'Date' fallita.");
         return NULL;
     }
+    // Inizializza i campi della data con i valori forniti
     date->minutes = minutes;
     date->hour = hour;
     date->day = day;
     date->month = month;
     date->year = year;
 
+    // Controlla se la data è valida
     if (!controllo_data(date))
     {
         printf("Tentativo di creazione date non valida\n");
@@ -37,6 +43,7 @@ Date nuova_data(int year, int month, int day, int hour, int minutes)
     return date;
 }
 
+// Funzione per controllare la validità di una data ritorna -1 in caso di errore, 0 se la data non è valida e 1 se la data è valida
 int controllo_data(Date date)
 {
     if (date == NULL)
@@ -73,19 +80,21 @@ int controllo_data(Date date)
     return 1;
 }
 
+// Funzione di utilità per confrontare due date
 int utile_data(int d1, int d2)
 {
     if (d1 < d2)
     {
-        return -1;
+        return -1; // d1 precede d2
     }
     if (d1 == d2)
     {
-        return 0;
+        return 0; // d1 è uguale a d2
     }
-    return 1;
+    return 1; // d1 segue d2
 }
 
+// Funzione per confrontare due date
 int confronta_date(Date data1, Date data2)
 {
     // Compare 'year'
@@ -116,14 +125,15 @@ int confronta_date(Date data1, Date data2)
     return utile_data(data1->minutes, data2->minutes);
 }
 
+// Funzioni getter per ottenere le componenti di una data
 int get_hour(Date date)
 {
     if (date == NULL)
     {
         printf("Passato puntatore NULL alla funzione 'get_hour'.");
-        return -1;
+        return -1; // Ritorna -1 in caso di errore
     }
-    return date->hour;
+    return date->hour; // Ritorna l'ora della data
 }
 
 int get_minutes(Date date)
@@ -131,19 +141,19 @@ int get_minutes(Date date)
     if (date == NULL)
     {
         printf("Passato puntatore NULL alla funzione 'get_minutes'.");
-        return -1;
+        return -1;  // Ritorna -1 in caso di errore
     }
-    return date->minutes;
-}
+    return date->minutes; // Ritorna i minuti della data
+} 
 
 int get_day(Date date)
 {
     if (date == NULL)
     {
         printf("Passato puntatore NULL alla funzione 'get_day'.");
-        return -1;
+        return -1;  // Ritorna -1 in caso di errore
     }
-    return date->day;
+    return date->day; // Ritorna il giorno della data
 }
 
 int get_month(Date date)
@@ -151,9 +161,9 @@ int get_month(Date date)
     if (date == NULL)
     {
         printf("Passato puntatore NULL alla funzione 'get_month'.");
-        return -1;
+        return -1;  // Ritorna -1 in caso di errore
     }
-    return date->month;
+    return date->month; // Ritorna il mese della data
 }
 
 int get_year(Date date)
@@ -161,17 +171,18 @@ int get_year(Date date)
     if (date == NULL)
     {
         printf("Passato puntatore NULL alla funzione 'get_year'.");
-        return -1;
+        return -1;  // Ritorna -1 in caso di errore
     }
-    return date->year;
+    return date->year; // Ritorna l'anno della data
 }
 
+// Funzione per inserire una data da input
 Date input_data(void)
 {
-    char temp[101] = {0};
+    char temp[101] = {0}; // Legge l'input dall'utente
     if (leggi_input(temp, 101))
     {
-        return NULL;
+        return NULL; // Ritorna NULL se c'è un errore nell'input
     }
 
     int day = 0;
@@ -180,20 +191,24 @@ Date input_data(void)
     int hour = 0;
     int minutes = 0;
 
+    //Trasforma l'input nel formato "giorno/mese/anno ora:minuti"
     if (sscanf(temp, "%d/%d/%d %d:%d", &day, &month, &year, &hour, &minutes) != 5)
     {
         return NULL;
     }
 
+    // Crea una nuova data con i valori trasformati
     Date date = nuova_data(year, month, day, hour, minutes);
-    return date;
+    return date; // Ritorna la nuova data
 }
 
+// Funzione per stampare una data
 void stampa_data(Date date)
 {
     printf("%d/%d/%d, %02d:%02d", date->day, date->month, date->year, date->hour, date->minutes);
 }
 
+// Funzione per liberare la memoria allocata per una data
 void free_date(Date date)
 {
     free(date);
