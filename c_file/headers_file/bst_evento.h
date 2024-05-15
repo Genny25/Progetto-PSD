7ddb27ab86b927c1d3e49398e3327f4b147785bf
Gennaro Carmine Tozza <gennarocarmine@gmail.com>
2024-05-15 12:40:34 +0200

    Update bst_evento.h

--- c_file/headers_file/bst_evento.h
+++ c_file/headers_file/bst_evento.h
@@ -13,6 +13,5 @@ int get_bst_size(eventBst bst);
 void stampa_evento_bst(eventBst bst, listaSala sala_lista);
 event bst_get_evento_by_id(eventBst bst, int id);
 void free_event_bst(eventBst bst);
-void salva_evento_bst_nodi(eventBstNode node, FILE *file);
 void salva_evento_bst_su_file(eventBst bst, FILE *file);
 eventBst leggi_evento_bst_da_file(FILE *file);
