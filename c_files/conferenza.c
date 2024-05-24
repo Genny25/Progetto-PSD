887a51beb988107a166a4e65b38be5be08fa4599
Lorenzo046 <147921555+Lorenzo046@users.noreply.github.com>
2024-05-24 13:07:28 +0200

    Update conferenza.c

--- c_files/conferenza.c
+++ c_files/conferenza.c
@@ -1,3 +1,5 @@
+
+
 // file conferenza.c
 
 #include <stdio.h>
@@ -20,81 +22,87 @@ Conferenza nuova_conferenza()
     eventoBst bst = nuovo_evento_bst(); // Crea un nuovo albero per gli eventi
     if (bst == NULL)
     {
-        return NULL;
+        return NULL; // Restituisce NULL se non è possibile allocare memoria per l'albero
     }
-    ListaSala sala_lista = nuova_lista_sala();
+    ListaSala sala_lista = nuova_lista_sala(); // Crea una nuova lista per le sale
     if (sala_lista == NULL)
     {
-        return NULL;
+        return NULL; // Restituisce NULL se non è possibile allocare memoria per la lista
     }
 
+    // Alloca memoria per la struttura della conferenza
     Conferenza conf = my_alloc(1, sizeof(*conf));
     if (conf == NULL)
     {
-        libera_evento_bst(bst);
-        return NULL;
+        libera_evento_bst(bst); // Libera la memoria dell'albero in caso di errore
+        return NULL; // Restituisce NULL se non è possibile allocare memoria per la conferenza
     }
+    // Inizializza i campi della conferenza
     conf->bst = bst;
     conf->sale = sala_lista;
     conf->id_evento = 0;
     conf->id_sala = 1;
 
-    return conf;
+    return conf; // Restituisce la nuova conferenza
 }
 
+// Funzione per aggiungere un nuovo evento alla conferenza
 int aggiungi_conferenza_evento(Conferenza conf)
 {
-    Evento evento = input_evento(conf->id_evento);
+    Evento evento = input_evento(conf->id_evento); // Ottiene un nuovo evento dall'input utente
     if (evento == NULL)
     {
-        return -1;
+        return -1; // Restituisce -1 se l'input è invalido o se si verifica un errore
     }
-    if (inserisci_evento_bst(conf->bst, evento))
+    if (inserisci_evento_bst(conf->bst, evento)) // Inserisce l'evento nell'albero degli eventi
     {
-        libera_evento(evento);
+        libera_evento(evento); // Libera la memoria dell'evento in caso di errore
     }
-    conf->id_evento++;
+    conf->id_evento++; // Incrementa l'ID degli eventi per la prossima aggiunta
 
-    return 0;
+    return 0; // Restituisce 0 in caso di successo
 }
 
+// Funzione per aggiungere una nuova sala alla conferenza
 int aggiungi_conferenza_sala(Conferenza conf)
 {
-    Sala s = input_sala(conf->id_sala);
+    Sala s = input_sala(conf->id_sala); // Ottiene una nuova sala dall'input utente
     if (s == NULL)
     {
-        return -1;
+        return -1; // Restituisce -1 se l'input è invalido o se si verifica un errore
     }
-    aggiungi_sala_lista(conf->sale, s);
-    conf->id_sala++;
-    return 0;
+    aggiungi_sala_lista(conf->sale, s); // Aggiunge la sala alla lista delle sale
+    conf->id_sala++; // Incrementa l'ID delle sale per la prossima aggiunta
+    return 0; // Restituisce 0 in caso di successo
 }
 
+// Funzione per rimuovere un evento dalla conferenza
 int rimuovi_conferenza_evento(Conferenza conf)
 {
-    stampa_evento_bst(conf->bst, conf->sale);
+    stampa_evento_bst(conf->bst, conf->sale); // Stampa gli eventi della conferenza
     int res;
     while (1)
     {
         printf("Inserisci l'id dell'evento da rimuovere [inserire un numero negativo per annullare l'operazione]:> ");
-        res = leggi_intero();
+        res = leggi_intero(); // Legge l'ID dell'evento dall'input utente
         if (res < 0)
         {
-            return 1;
+            return 1; // Esce se l'utente sceglie di annullare l'operazione
         }
 
-        Evento rimuovi = bst_rimuovi_evento_by_id(conf->bst, res);
+        Evento rimuovi = bst_rimuovi_evento_by_id(conf->bst, res); // Rimuove l'evento dall'albero
         if (rimuovi == NULL)
         {
             printf("L'evento non è presente nella conferenza\n");
-            return -1;
+            return -1; // Restituisce -1 se l'evento non è presente nella conferenza
         }
-        libera_evento(rimuovi);
+        libera_evento(rimuovi); // Libera la memoria dell'evento rimosso
         printf("Evento rimosso correttamente\n");
-        return 0;
+        return 0; // Restituisce 0 in caso di successo
     }
 }
 
+// Funzione per modificare il nome di un evento nella conferenza
 int modifica_nome_conferenza_evento(Conferenza conf, Evento evento)
 {
     char temp[101];
@@ -102,40 +110,43 @@ int modifica_nome_conferenza_evento(Conferenza conf, Evento evento)
     if (leggi_input(temp, sizeof(temp)))
     {
         puts("Nome non valido");
-        return -1;
+        return -1; // Restituisce -1 se l'input del nome è invalido
     }
 
-    if (imposta_nome(evento, temp))
+    if (imposta_nome(evento, temp)) // Imposta il nuovo nome per l'evento
     {
         puts("Errore nella modifica del nome");
-        return -1;
+        return -1; // Restituisce -1 in caso di errore
     }
 
-    return 0;
+    return 0; // Restituisce 0 in caso di successo
 }
 
+// Funzione per modificare la data di inizio di un evento nella conferenza
 int modifica_data_inizio_conferenza_evento(Conferenza conf, Evento evento)
 {
     Data data = NULL;
     do
     {
         printf("Inserisci data inizio evento (DD/MM/AAAA hh:mm):> ");
-        data = input_data();
-    } while (data == NULL && printf("Data inserita non valida\n"));
-    return imposta_data_inizio(evento, data);
+        data = input_data(); // Legge la nuova data di inizio dall'input utente
+    } while (data == NULL && printf("Data inserita non valida\n")); // Ripete finché l'input non è valido
+    return imposta_data_inizio(evento, data); // Imposta la nuova data di inizio per l'evento
 }
 
+// Funzione per modificare la data di fine di un evento nella conferenza
 int modifica_data_fine_conferenza_evento(Conferenza conf, Evento evento)
 {
     Data data = NULL;
     do
     {
         printf("Inserisci data fine evento (DD/MM/AAAA hh:mm):> ");
-        data = input_data();
-    } while (data == NULL && printf("Data inserita non valida\n"));
-    return imposta_data_fine(evento, data);
+        data = input_data(); // Legge la nuova data di fine dall'input utente
+    } while (data == NULL && printf("Data inserita non valida\n")); // Ripete finché l'input non è valido
+    return imposta_data_fine(evento, data); // Imposta la nuova data di fine per l'evento
 }
 
+// Funzione per modificare il tipo di un evento nella conferenza
 int modifica_tipo_conferenza_evento(Conferenza conf, Evento evento)
 {
     int tipo;
@@ -146,39 +157,40 @@ int modifica_tipo_conferenza_evento(Conferenza conf, Evento evento)
         printf("[2]: Sessione di keynote\n");
         printf("[3]: Panel di discussione\n");
         printf("Seleziona tipologia evento (1, 2 o 3):> ");
-        scanf("%d", &tipo);
+        scanf("%d", &tipo); // Legge il tipo di evento dall'input utente
         pulisci_buffer(stdin);
-    } while (tipo < 1 || tipo > 3);
+    } while (tipo < 1 || tipo > 3); // Ripete finché l'input non è valido
 
-    if (imposta_tipo(evento, tipo))
+    if (imposta_tipo(evento, tipo)) // Imposta il nuovo tipo per l'evento
     {
         puts("Errore nella modifica del tipo");
-        return -1;
+        return -1; // Restituisce -1 in caso di errore
     }
 
-    return 0;
+    return 0; // Restituisce 0 in caso di successo
 }
 
+// Funzione per modificare un evento nella conferenza
 int modifica_conferenza_evento(Conferenza conf)
 {
     int res, scelta, flag = 1;
 
-    stampa_evento_bst(conf->bst, conf->sale);
+    stampa_evento_bst(conf->bst, conf->sale); // Stampa gli eventi della conferenza
 
     while (1)
     {
         printf("Inserisci l'id dell'evento da modificare [inserire un numero negativo per annullare l'operazione]:> ");
-        res = leggi_intero();
+        res = leggi_intero(); // Legge l'ID dell'evento dall'input utente
         if (res < 0)
         {
-            return 1;
+            return 1; // Esce se l'utente sceglie di annullare l'operazione
         }
 
-        Evento modifica = bst_rimuovi_evento_by_id(conf->bst, res);
+        Evento modifica = bst_rimuovi_evento_by_id(conf->bst, res); // Rimuove l'evento dall'albero
         if (modifica == NULL)
         {
             printf("Errore nella ricerca dell'evento\n");
-            return -1;
+            return -1; // Restituisce -1 se si verifica un errore nella ricerca dell'evento
         }
 
         do
@@ -190,7 +202,7 @@ int modifica_conferenza_evento(Conferenza conf)
             printf("4 - Tipo\n");
             printf("5-  Esci dal menu\n");
             printf("Inserisci il campo da modificare:> ");
-            scanf("%d", &scelta);
+            scanf("%d", &scelta); // Legge la scelta dall'input utente
             pulisci_buffer(stdin);
             switch (scelta)
             {
@@ -210,39 +222,42 @@ int modifica_conferenza_evento(Conferenza conf)
                 printf("Errore nella scelta\n");
                 break;
             }
-        } while (scelta != 5);
+        } while (scelta != 5); // Continua finché l'utente non sceglie di uscire
 
         if (flag)
         {
             printf("\n\n");
-            Sala s = sala_per_id_lista(conf->sale, prendi_evento_sala_id(modifica));
-            stampa_evento(modifica, s);
+            Sala s = sala_per_id_lista(conf->sale, prendi_evento_sala_id(modifica)); // Ottiene la sala dell'evento modificato
+            stampa_evento(modifica, s); // Stampa le informazioni dell'evento modificato
             printf("\n\n");
         }
 
-        if (inserisci_evento_bst(conf->bst, modifica))
+        if (inserisci_evento_bst(conf->bst, modifica)) // Reinserisce l'evento nell'albero
         {
-            libera_evento(modifica);
+            libera_evento(modifica); // Libera la memoria dell'evento in caso di errore
             puts("Errore durante la modifica");
-            return -1;
+            return -1; // Restituisce -1 in caso di errore
         }
         puts("Evento modifica correttamente");
-        return 0;
+        return 0; // Restituisce 0 in caso di successo
     }
 }
 
+// Funzione per stampare tutti gli eventi della conferenza
 int stampa_conferenza(Conferenza conf)
 {
-    stampa_evento_bst(conf->bst, conf->sale);
-    return 0;
+    stampa_evento_bst(conf->bst, conf->sale); // Stampa gli eventi della conferenza
+    return 0; // Restituisce 0 in caso di successo
 }
 
+// Funzione per stampare tutte le sale della conferenza
 int stampa_conferenza_sale(Conferenza conf)
 {
-    stampa_lista_sala(conf->sale);
-    return 0;
+    stampa_lista_sala(conf->sale); // Stampa le sale della conferenza
+    return 0; // Restituisce 0 in caso di successo
 }
 
+// Funzione per assegnare un evento a una sala nella conferenza
 int conferenza_assegna_evento_a_sala(Conferenza conf)
 {
     int res = 0;
@@ -250,116 +265,119 @@ int conferenza_assegna_evento_a_sala(Conferenza conf)
     if (prendi_dimensione_lista_sala(conf->sale) == 0)
     {
         printf("Non ci sono sale disponibili.\n");
-        return -1;
+        return -1; // Restituisce -1 se non ci sono sale disponibili
     }
-    stampa_evento_bst(conf->bst, conf->sale);
+    stampa_evento_bst(conf->bst, conf->sale); // Stampa gli eventi della conferenza
     while (1)
     {
         printf("Inserisci l'id dell'evento da assegnare [inserire un numero negativo per annullare l'operazione]:> ");
-        res = leggi_intero();
+        res = leggi_intero(); // Legge l'ID dell'evento dall'input utente
         if (res < 0)
         {
-            return 1;
+            return 1; // Esce se l'utente sceglie di annullare l'operazione
         }
 
-        evento = bst_prendi_evento_by_id(conf->bst, res);
+        evento = bst_prendi_evento_by_id(conf->bst, res); // Ottiene l'evento dall'albero
 
-        stampa_lista_sala(conf->sale);
+        stampa_lista_sala(conf->sale); // Stampa le sale disponibili
 
         int pos = 0;
         do
         {
             printf("Inserisci numero sala: ");
-            int res_sala = leggi_intero();
+            int res_sala = leggi_intero(); // Legge il numero della sala dall'input utente
             pos = res_sala - 1;
 
-        } while ((pos < 0 || pos >= prendi_dimensione_lista_sala(conf->sale)) && printf("Numero sala inseriro non valido\n"));
+        } while ((pos < 0 || pos >= prendi_dimensione_lista_sala(conf->sale)) && printf("Numero sala inseriro non valido\n")); // Ripete finché l'input non è valido
 
-        Sala s = sala_in_posizione_lista(conf->sale, pos);
+        Sala s = sala_in_posizione_lista(conf->sale, pos); // Ottiene la sala dalla lista
         if (s == NULL)
         {
             printf("Qualcosa è andato storto durante la ricerca della sala\n");
-            return -2;
+            return -2; // Restituisce -2 se si verifica un errore durante la ricerca della sala
         }
 
-        imposta_evento_sala_id(evento, prendi_sala_id(s));
+        imposta_evento_sala_id(evento, prendi_sala_id(s)); // Imposta l'ID della sala per l'evento
         printf("Sala assegnata con successo\n");
 
-        return 0;
+        return 0; // Restituisce 0 in caso di successo
     }
 }
 
+// Funzione per salvare la conferenza su un file
 void salva_conferenza_su_file(Conferenza conf, FILE *file)
 {
     if (conf == NULL || file == NULL)
     {
         perror("Invalid conference or file pointer");
-        return;
+        return; // Esce se la conferenza o il file sono invalidi
     }
 
-    // Save event ID counter and room ID counter
+    // Salva i contatori degli ID degli eventi e delle sale
     fprintf(file, "%d %d\n", conf->id_evento, conf->id_sala);
 
-    // Save events
+    // Salva gli eventi
     salva_evento_bst_su_file(conf->bst, file);
 
-    // Save rooms
+    // Salva le sale
     salva_lista_sala_su_file(conf->sale, file);
 }
 
+// Funzione per leggere la conferenza da un file
 Conferenza leggi_conferenza_da_file(FILE *file)
 {
     if (file == NULL)
     {
         perror("Invalid file pointer");
-        return NULL;
+        return NULL; // Esce se il file è invalido
     }
 
-    // Read event ID counter and room ID counter
+    // Legge i contatori degli ID degli eventi e delle sale
     int evento_id = 0;
     int sala_id = 0;
     if (fscanf(file, "%d %d", &evento_id, &sala_id) != 2)
     {
         perror("Error reading event ID counter and room ID counter");
-        return NULL;
+        return NULL; // Esce se si verifica un errore nella lettura degli ID
     }
     pulisci_buffer(file);
 
-    // Create a new conference
+    // Crea una nuova conferenza
     Conferenza conf = nuova_conferenza();
     if (conf == NULL)
     {
         perror("Error creating new conference");
-        return NULL;
+        return NULL; // Esce se si verifica un errore nella creazione della conferenza
     }
 
     conf->id_evento = evento_id;
     conf->id_sala = sala_id;
 
-    // Read events
+    // Legge gli eventi
     conf->bst = leggi_evento_bst_da_file(file);
     if (conf->bst == NULL)
     {
         perror("Error reading events");
         libera_conferenza(conf);
-        return NULL;
+        return NULL; // Esce se si verifica un errore nella lettura degli eventi
     }
 
-    // Read rooms
+    // Legge le sale
     conf->sale = leggi_lista_sala_da_file(file);
     if (conf->sale == NULL)
     {
         perror("Error reading rooms");
         libera_conferenza(conf);
-        return NULL;
+        return NULL; // Esce se si verifica un errore nella lettura delle sale
     }
 
-    return conf;
+    return conf; // Restituisce la conferenza letta dal file
 }
 
+// Funzione per liberare la memoria allocata per la conferenza
 void libera_conferenza(Conferenza conf)
 {
-    libera_evento_bst(conf->bst);
-    libera_lista_sala(conf->sale);
-    free(conf);
+    libera_evento_bst(conf->bst);  // Libera l'albero degli eventi
+    libera_lista_sala(conf->sale); // Libera la lista delle sale
+    free(conf);  // Libera la memoria della struttura conferenza
 }
